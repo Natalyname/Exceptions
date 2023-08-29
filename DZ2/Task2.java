078017dec637132287a3b3244aa524ad7a0880f1
Nataly <120113884+Natalyname@users.noreply.github.com>
2023-08-29 18:16:52 +0300

    added Task2.java

--- DZ2/Task2.java
+++ DZ2/Task2.java
@@ -1,7 +1,7 @@
 // Если необходимо, исправьте данный код
 // (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
 
-package homework2;
+package DZ2;
 
 import java.util.Random;
 
