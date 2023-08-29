c1c0320bf36cf6aa43f8091f3f6cc0bdaa3f3d70
Nataly <120113884+Natalyname@users.noreply.github.com>
2023-08-29 18:16:24 +0300

    added Task3.java

--- DZ2/Task3.java
+++ DZ2/Task3.java
@@ -1,7 +1,7 @@
 // Дан следующий код, исправьте его там, где требуется
 // (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
 
-package homework2;
+package DZ2;
 
 public class Task3 {
 
